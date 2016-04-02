b3c7ca9d1379a3f22c3955d39f932a6803b6279c
Michael Brown <brownieboy@gmail.com>
2016-04-02 18:55:33 +1100

    Reimplemented webview source prop correctly as an object

--- components/webpage.js
+++ components/webpage.js
@@ -1,10 +1,10 @@
 var React = require('react-native');
 var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  WebView
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    WebView
 } = React;
 
 var Button = require('react-native-button');
@@ -13,16 +13,16 @@ var GiftedSpinner = require('react-native-gifted-spinner');
 var _ = require('lodash');
 
 var WebPage = React.createClass({
-	getInitialState: function() {
-		return {
-			isLoading: true
-		};
-	},
+    getInitialState: function() {
+        return {
+            isLoading: true
+        };
+    },
 
-	render: function(){
-			
-		return (<View style={styles.container}>
-		
+    render: function() {
+        var sourceObj = { url: this.props.url };
+        return (<View style={styles.container}>
+
 			<View style={styles.webview_header}>
 			  <View style={styles.header_item}>
 			  	<Button style={styles.button} onPress={this.back}>Back</Button>
@@ -36,65 +36,65 @@ var WebPage = React.createClass({
 			</View>
 
 			<View style={styles.webview_body}>
-				<WebView 
-					url={this.props.url}
+				<WebView
+					source={sourceObj}
 					onNavigationStateChange={this.onNavigationStateChange}
-					
+
 				/>
 			</View>
 		</View>);
 
-	},
+    },
 
-	truncate: function(str){
-		return _.truncate(str, 20);
-	},
+    truncate: function(str) {
+        return _.truncate(str, 20);
+    },
 
-	onNavigationStateChange: function(navState) {
-		
-		if(!navState.loading){
-			this.setState({
-				isLoading: false,
-				pageTitle: navState.title
-			});
-		}
-	},
-	
-	back: function(){
-	   this.props.navigator.pop();
-  	}
+    onNavigationStateChange: function(navState) {
+
+        if (!navState.loading) {
+            this.setState({
+                isLoading: false,
+                pageTitle: navState.title
+            });
+        }
+    },
+
+    back: function() {
+        this.props.navigator.pop();
+    }
 });
 
 
 var styles = StyleSheet.create({
-	container: {
-		flex: 1
-	},
-	webview_header: {
-		paddingLeft: 10,
-		backgroundColor: '#FF6600',
-		flex: 1,
-		justifyContent: 'space-between',
-		flexDirection: 'row'
-	},
-	header_item: {
-		paddingLeft: 10,
-		paddingRight: 10,
-		justifyContent: 'center'
-	},
-	webview_body: {
-		flex: 9
-	},
-	button: {
-		textAlign: 'left',
-		color: '#FFF'
-	},
-	page_title: {
-		color: '#FFF'
-	},
-	spinner: {
-		alignItems: 'flex-end'
-	}
+    container: {
+        flex: 1
+    },
+    webview_header: {
+        paddingLeft: 10,
+        backgroundColor: '#FF6600',
+        flex: 1,
+        justifyContent: 'space-between',
+        flexDirection: 'row'
+    },
+    header_item: {
+        paddingLeft: 10,
+        paddingRight: 10,
+        justifyContent: 'center'
+    },
+    webview_body: {
+        flex: 9
+    },
+    button: {
+        textAlign: 'left',
+        color: '#FFF'
+    },
+    page_title: {
+        color: '#FFF'
+    },
+    spinner: {
+        alignItems: 'flex-end'
+    }
 });
 
 module.exports = WebPage;
